76995222515ff5566902be29f2f3c01296fa5676
Prantar Borah <105645647+PrantarBorah@users.noreply.github.com>
2025-06-07 00:06:13 -0400

    Fix navigation and model path issues

--- Multi_DiseasePrediction_Streamlit.py
+++ Multi_DiseasePrediction_Streamlit.py
@@ -9,12 +9,13 @@ st.set_page_config(page_title="Health Assistant",
                    page_icon="🧑‍⚕️")
 
 # loading the saved models
+BASE_DIR = os.path.dirname(__file__)
 
-diabetes_model = pickle.load(open('/Users/prantarborah/Downloads/trained_model.sav', 'rb'))
+diabetes_model = pickle.load(open(os.path.join(BASE_DIR, 'trained_model.sav'), 'rb'))
 
-heart_disease_model = pickle.load(open('/Users/prantarborah/Downloads//heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open(os.path.join(BASE_DIR, 'heart_disease_model.sav'), 'rb'))
 
-parkinsons_model = pickle.load(open('/Users/prantarborah/Downloads//parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open(os.path.join(BASE_DIR, 'parkinsons_model.sav'), 'rb'))
 
 # sidebar for navigation
 with st.sidebar:
@@ -83,7 +84,7 @@ if selected == 'Diabetes Prediction':
 
     st.success(diab_diagnosis)
 
-    # Heart Disease Prediction Page
+# Heart Disease Prediction Page
 if selected == 'Heart Disease Prediction':
 
     # page title
@@ -150,101 +151,101 @@ if selected == 'Heart Disease Prediction':
 
     st.success(heart_diagnosis)
 
-    # Parkinson's Prediction page
-    if selected =="Parkinsons Prediction":
-        
-        #pagetitle
-        st.title("Parkinsons Disease Prediction using ML")
+# Parkinson's Prediction page
+if selected == "Parkinsons Prediction":
 
-        col1,col2,col3,col4,col5 = st.columns(5)
+    # page title
+    st.title("Parkinsons Disease Prediction using ML")
 
-        with col1:
-            fo = st.text_input('MDVP:Fo(Hz)')
+    col1, col2, col3, col4, col5 = st.columns(5)
 
-        with col2:
-            fhi = st.text_input('MDVP:Fhi(Hz)')
+    with col1:
+        fo = st.text_input('MDVP:Fo(Hz)')
 
-        with col3:
-            flo = st.text_input('MDVP:Flo(Hz)')
+    with col2:
+        fhi = st.text_input('MDVP:Fhi(Hz)')
 
-        with col4:
-            Jitter_percent = st.text_input('MDVP:Jitter(%)')
+    with col3:
+        flo = st.text_input('MDVP:Flo(Hz)')
 
-        with col5:
-            Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
+    with col4:
+        Jitter_percent = st.text_input('MDVP:Jitter(%)')
 
-        with col1:
-            RAP = st.text_input('MDVP:RAP')
+    with col5:
+        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
 
-        with col2:
-            PPQ = st.text_input('MDVP:PPQ')
+    with col1:
+        RAP = st.text_input('MDVP:RAP')
 
-        with col3:
-            DDP = st.text_input('Jitter:DDP')
+    with col2:
+        PPQ = st.text_input('MDVP:PPQ')
 
-        with col4:
-            Shimmer = st.text_input('MDVP:Shimmer')
+    with col3:
+        DDP = st.text_input('Jitter:DDP')
 
-        with col5:
-            Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
+    with col4:
+        Shimmer = st.text_input('MDVP:Shimmer')
 
-        with col1:
-            APQ3 = st.text_input('Shimmer:APQ3')
+    with col5:
+        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
 
-        with col2:
-            APQ5 = st.text_input('Shimmer:APQ5')
+    with col1:
+        APQ3 = st.text_input('Shimmer:APQ3')
 
-        with col3:
-            APQ = st.text_input('MDVP:APQ')
+    with col2:
+        APQ5 = st.text_input('Shimmer:APQ5')
 
-        with col4:
-            DDA = st.text_input('Shimmer:DDA')
+    with col3:
+        APQ = st.text_input('MDVP:APQ')
 
-        with col5:
-            NHR = st.text_input('NHR')
+    with col4:
+        DDA = st.text_input('Shimmer:DDA')
 
-        with col1:
-            HNR = st.text_input('HNR')
+    with col5:
+        NHR = st.text_input('NHR')
 
-        with col2:
-            RPDE = st.text_input('RPDE')
+    with col1:
+        HNR = st.text_input('HNR')
 
-        with col3:
-            DFA = st.text_input('DFA')
+    with col2:
+        RPDE = st.text_input('RPDE')
 
-        with col4:
-            spread1 = st.text_input('spread1')
+    with col3:
+        DFA = st.text_input('DFA')
 
-        with col5:
-            spread2 = st.text_input('spread2')
+    with col4:
+        spread1 = st.text_input('spread1')
 
-        with col1:
-            D2 = st.text_input('D2')
+    with col5:
+        spread2 = st.text_input('spread2')
 
-        with col2:
-            PPE = st.text_input('PPE')
-        
+    with col1:
+        D2 = st.text_input('D2')
 
-        # code for Prediction
-        parkinsons_diagnosis = ''
+    with col2:
+        PPE = st.text_input('PPE')
+    
 
-        # creating a button for Prediction    
-        if st.button("Parkinson's Test Result"):
+    # code for Prediction
+    parkinsons_diagnosis = ''
 
-            user_input = [fo, fhi, flo, Jitter_percent, Jitter_Abs,
-                        RAP, PPQ, DDP,Shimmer, Shimmer_dB, APQ3, APQ5,
-                         APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]
+    # creating a button for Prediction    
+    if st.button("Parkinson's Test Result"):
 
-            user_input = [float(x) for x in user_input]
+        user_input = [fo, fhi, flo, Jitter_percent, Jitter_Abs,
+                    RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5,
+                     APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]
 
-            parkinsons_prediction = parkinsons_model.predict([user_input])
+        user_input = [float(x) for x in user_input]
 
-            if parkinsons_prediction[0] == 1:
-                parkinsons_diagnosis = "The person has Parkinson's disease"
-            else:
-                parkinsons_diagnosis = "The person does not have Parkinson's disease"
+        parkinsons_prediction = parkinsons_model.predict([user_input])
 
-        st.success(parkinsons_diagnosis)
-        
+        if parkinsons_prediction[0] == 1:
+            parkinsons_diagnosis = "The person has Parkinson's disease"
+        else:
+            parkinsons_diagnosis = "The person does not have Parkinson's disease"
+
+    st.success(parkinsons_diagnosis)
+    
         
 
